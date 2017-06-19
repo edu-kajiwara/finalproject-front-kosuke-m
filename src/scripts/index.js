bf3fe4e640f8dd48acbd01125f8fcb1f32b9a112
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2017-06-20 05:21:43 +0900

    暫定Webpack2対応

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,3 @@
-require("materialize-loader");
-require('materialize-css/dist/js/materialize.js');
 require('./app.tag');
 require('./users.tag');
 require('./cardpanel.tag');
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,79 @@
-var webpack = require("webpack");
-var InlineEnviromentVariablesPlugin = require('inline-environment-variables-webpack-plugin');
-console.log(process.env);
 const path = require('path');
-module.exports = {
-  entry: './src/scripts/index.js',
-  output: {
-    path: path.join(__dirname, 'app', 'scripts'),
-    filename: 'bundle.js',
-  },
-  module: {
-    preLoaders: [
-      {
-        test: /\.tag$/,
-        exclude: /node_modules/,
-        loader: 'riotjs-loader',
-        query: {
-          type: 'babel'
-        }
-      }
-    ],
-    loaders: [
-      {
-        test: /\.js$|\.tag$/,
-        exclude: /node_modules/,
-        loader: 'babel'
-      },
-      {test: /\.scss$/,                     loader: 'style!css!sass'},
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader:"url" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file" }
-    ]
-  },
-  resolve: {
-      extensions: ['', '.js', '.tag']
-  },
-  plugins: [
-    new InlineEnviromentVariablesPlugin(process.env),
-    new webpack.ProvidePlugin({
-      riot: 'riot',
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    })
-  ]
-}
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports =  [
+    {
+        // context: path.join(__dirname, './src'),
+        entry: {
+            app: './src/scripts/index.js',
+            vendor: [
+            // /path/to/の部分に設定ファイルまでのパスを指定する
+            "materialize-loader!./materialize.config.js",
+            ]
+        },
+        output: {
+            path: path.join(__dirname, 'app'),
+            // filename: 'bundle.js'
+            filename: "scripts/[name].js",
+        },
+        module: {
+            rules: [
+                { 
+                    test: /\.scss$/, 
+                    use: ExtractTextPlugin.extract({
+                        fallback: "style-loader",
+                        use: ["css-loader", "postcss-loader", "sass-loader"]
+                    })
+                },
+                { 
+                    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                    loader:"url-loader", 
+                    query: { 
+                        limit: 10000, 
+                        mimetype: "application/font-woff" 
+                    }
+                },
+                { 
+                    test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                    loader: "file-loader" 
+                },
+                {
+                    test: /\.tag$/,
+                    exclude: /node_modules/,
+                    enforce: 'pre',
+                    use: [
+                        {
+                            loader: 'riot-tag-loader',
+                            options: {
+                                debug: true
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.js$|\.tag$/,
+                    enforce: 'post',
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: `es2015-riot`
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['*', '.js', '.tag']
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                riot: 'riot',
+                fetch: "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch",
+            })
+        ]
+    }
+];
