6a5f2dfa2c2c316d1319ff302ff06aaaab8f5a3a
KAJIWARA Yutaka <kajiwara.yutaka@classmethod.jp>
2017-06-27 04:24:23 +0900

    Materializeからbootstrapへ変更

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,8 @@ module.exports =  [
             app: './src/scripts/index.js',
             vendor: [
             // /path/to/の部分に設定ファイルまでのパスを指定する
-            "materialize-loader!./materialize.config.js",
-            ]
+            'bootstrap-loader'
+            ],
         },
         output: {
             path: path.join(__dirname, 'app'),
@@ -21,13 +21,6 @@ module.exports =  [
         },
         module: {
             rules: [
-                { 
-                    test: /\.scss$/, 
-                    use: ExtractTextPlugin.extract({
-                        fallback: "style-loader",
-                        use: ["css-loader", "postcss-loader", "sass-loader"]
-                    })
-                },
                 { 
                     test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
                     loader:"url-loader", 
@@ -76,6 +69,22 @@ module.exports =  [
             new webpack.ProvidePlugin({
                 riot: 'riot',
                 fetch: "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch",
+                    $: "jquery",
+    jQuery: "jquery",
+    "window.jQuery": "jquery",
+    Tether: "tether",
+    "window.Tether": "tether",
+    Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+    Button: "exports-loader?Button!bootstrap/js/dist/button",
+    Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+    Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+    Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+    Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+    Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+    Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+    Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+    Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+    Util: "exports-loader?Util!bootstrap/js/dist/util",
             })
         ]
     }
